777de2c8e793702e32fde7ef7764e863fb373e68
ivan <ivanminutillo@outlook.it>
2025-01-23 11:46:42 +0100

    tw4

--- lib/web/components/set/boundary_composer_actions_live.ex
+++
@@ -1,6 +0,0 @@
-defmodule Bonfire.UI.Boundaries.BoundaryComposerActionsLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop done_label, :string, default: nil
-  prop hide_preview, :boolean, default: nil
-end

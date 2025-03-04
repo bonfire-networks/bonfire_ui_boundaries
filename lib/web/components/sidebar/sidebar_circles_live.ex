7ce9d98104d683698f47cc1bdb0bb94712c6a3be
Mayel de Borniol <git@mayel.space>
2025-03-04 15:40:05 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/759

--- lib/web/components/sidebar/sidebar_circles_live.ex
+++ lib/web/components/sidebar/sidebar_circles_live.ex
@@ -2,7 +2,9 @@ defmodule Bonfire.UI.Boundaries.SidebarCirclesLive do
   use Bonfire.UI.Common.Web, :stateful_component
   alias Bonfire.Boundaries.LiveHandler
 
-  declare_nav_component("Links to user's personal and subscribed circles", exclude_from_nav: false)
+  declare_nav_component("Links to user's personal and subscribed circles",
+    exclude_from_nav: false
+  )
 
   def update(assigns, socket) do
     # scope = LiveHandler.scope_origin(assigns, socket)
@@ -16,9 +18,8 @@ defmodule Bonfire.UI.Boundaries.SidebarCirclesLive do
        loaded: true,
        circles: edges,
        page_info: page_info
-      #  settings_section_title: "Create and manage your circles",
-      #  settings_section_description: "Create and manage your circles."
+       #  settings_section_title: "Create and manage your circles",
+       #  settings_section_description: "Create and manage your circles."
      )}
   end
-
 end

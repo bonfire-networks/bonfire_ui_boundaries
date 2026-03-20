91cf792ec6b70780724b8428c9cfe653f216db65
ivan <ivanminutillo@outlook.it>
2026-03-20 11:46:41 +0100

    boundary

--- lib/web/components/roles/your_role_live.ex
+++ lib/web/components/roles/your_role_live.ex
@@ -2,7 +2,7 @@ defmodule Bonfire.UI.Boundaries.YourRoleLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop role_name, :string, required: true
-  prop label, :string, default: nil
+  prop label, :string, default: "Your permissions"
   prop role_permissions, :any, default: nil
   prop is_caretaker, :boolean, default: true
   prop scope, :any, default: nil

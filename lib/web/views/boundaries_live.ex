1343ae98cdfd73e88d989d04f252e8d32b09ec33
ivan <ivanminutillo@outlook.it>
2026-05-04 10:29:38 +0200

    Update boundaries_live.ex

--- lib/web/views/boundaries_live.ex
+++ lib/web/views/boundaries_live.ex
@@ -5,7 +5,7 @@ defmodule Bonfire.UI.Boundaries.BoundariesLive do
   # alias Bonfire.Boundaries.Circles
 
   declare_extension(
-    "UI for boundaries",
+    "Boundaries defaults",
     icon: "fa6-solid:handshake-angle",
     emoji: "🤝",
     description:
